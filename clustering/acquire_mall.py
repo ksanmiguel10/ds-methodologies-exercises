8ef2ff2ffe7700a08eb9f29e76836cd9268d9687
Kathryn Salts <kathrynsalts@gmail.com>
2019-04-09 12:08:03 -0500

    initial

---
+++ clustering/acquire_mall.py
@@ -0,0 +1,11 @@
+import env
+import pandas as pd
+
+def get_connection(db, user=env.user, host=env.host, password=env.password):
+    return f'mysql+pymysql://{user}:{password}@{host}/{db}'
+
+def get_mall_data():
+    df = pd.read_sql('\
+    SELECT * FROM customers;',\
+    get_connection('mall_customers'))
+    return df
