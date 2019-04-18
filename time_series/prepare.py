4150053c77462541b492a48ff6425e9b59d0d5a5
Kathryn Salts <kathrynsalts@gmail.com>
2019-04-18 15:51:28 -0500

    added more

--- time_series/prepare.py
+++ time_series/prepare.py
@@ -4,22 +4,37 @@ import pandas as pd
 import numpy as np
 import acquire
 
-def make_datetime(df[col]):
-    pd.to_datetime(df[col], utc=True)
+def make_datetime(df):
+    datetime_format = '%a, %d %b %Y %H:%M:%S %Z'
+    df['sale_date'] = pd.to_datetime(df['sale_date'], format=datetime_format, utc=True)
+    return df
 
-def split_date_series(series: pd.Series) -> tuple:
-    """
-    Return six columns: year, quarter, month, day, weekday #, and whether it's a weekday (1) or weekend (0)
-    """
-    year = series.dt.year.rename("year")
-    quarter = series.dt.quarter.rename("quarter")
-    month = series.dt.month.rename("month")
-    day = series.dt.day.rename("day")
-    weekday = series.dt.weekday.rename("weekday")
-    is_weekday = (series.dt.weekday < 5).rename("is_weekday")
-    return year, quarter, month, day, weekday, is_weekday
+def add_date_parts(df):
+    df['year'] = df.sale_date.dt.year
+    df['quarter'] = df.sale_date.dt.quarter
+    df['month'] = df.sale_date.dt.month
+    df['day'] = df.sale_date.dt.day
+    df['weekday'] = df.sale_date.dt.day_name().str[:3]
+    df['is_weekend'] = df.weekday.str.startswith('S')
+    return df
 
-# df = pd.concat([df, *split_date_series(df.sale_date)], axis=1)
+def make_sales_total(df):
+    df['sale_total'] = df.sale_amount * df.item_price
+    return df.rename(columns={'sale_amount':'quantity'})
 
-def make_dt_index(df[col]):
-    df.set_index(df[col], inplace=True)
+def add_sales_difference(df):
+    df['diff_from_last_day'] = df.sale_total.diff()
+    return df
+
+def make_dt_index(df):
+    return df.set_index('sale_date', inplace=True)
+
+def aggregate_by_weekday(df):
+    return df.groupby('weekday')[['sale_total', 'quantity']].agg(['median', 'sum'])
+
+def prep_store_data(df):
+    df = make_datetime(df)
+    df = add_date_parts(df)
+    df = make_sales_total(df)
+    df = add_sales_difference(df)
+    return df
